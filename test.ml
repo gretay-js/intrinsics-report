f2b35460ebf8d7dedcd3eeaa6a78b21eda391fd6
Greta Yorsh <gyorsh@janestreet.com>
2022-04-25 17:40:53 +0100

    comment just to trigger another ci build

--- test.ml
+++ test.ml
@@ -5,6 +5,12 @@ let pp msg n =
   Printf.printf "%s: %d\n" msg n;
   n
 
+(*
+ fails on github ci and old macs (e.g. macbook air from 2012, with Intel Core i5-3317U CPU0
+ with first release of ocaml_intrinsics which incorrectly passes -mbmi and -mlzcnt
+ to c compiler, even though they aren't available on the target,
+ and the c compiler generates BEXTR and LZCNT instructions for int_stubs. 
+ *) 
 let suite = "Intrinsics tests" >::: [
   "clz"     >:: (fun _ -> assert_equal 28 (count_leading_zeros 0x400000000 |> pp "clz"));
   "clz2"    >:: (fun _ -> assert_equal 28 (count_leading_zeros2 0x400000000 |> pp "clz2"));
